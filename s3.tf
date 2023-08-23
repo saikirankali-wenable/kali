7c0a94d0233c411ab589cbc9a4c0687b27e95032
saikirankali-wenable <saikiran.kali@wenable.com>
2023-08-23 16:50:04 +0530

    third commit

---
+++ s3.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "onebucket" {
+   bucket = "testing-s3-with-terraform"
+   acl = "private"
+   versioning {
+      enabled = true
+   }
+   tags = {
+     Name = "Bucket1"
+     
+   }
+}
+    
+
